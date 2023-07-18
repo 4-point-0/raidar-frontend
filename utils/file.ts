07ba421e68466193aa4f2e88420f323b4c5d73be
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-07-18 11:21:18 +0200

    Refactoring file.ts

--- utils/file.ts
+++ utils/file.ts
@@ -1,6 +1,6 @@
 import { FileDto } from "@/services/api/artist/artistSchemas";
 
-export function getImageUrl(file?: FileDto) {
+export function getFileUrl(file?: FileDto) {
   if (!file) return;
 
   return `${file.url}?updatedAt=${file.updated_at}`;
