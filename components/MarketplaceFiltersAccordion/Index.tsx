fa5855c7651635e707fbaffb7018e30ba7c4d1df
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-07-31 15:17:36 +0200

    Marketplace Filter - initial funcionality

--- components/MarketplaceFiltersAccordion/Index.tsx
+++ components/MarketplaceFiltersAccordion/Index.tsx
@@ -1,5 +1,14 @@
-import { Accordion, Grid, Select, rem, createStyles } from "@mantine/core";
-import React from "react";
+import {
+  Accordion,
+  Grid,
+  Select,
+  rem,
+  createStyles,
+  Button,
+} from "@mantine/core";
+import { useState } from "react";
+import { useMarketplaceControllerFindAll } from "../../services/api/components";
+import { useSession } from "next-auth/react";
 
 const useStyles = createStyles((theme) => ({
   item: {
@@ -12,8 +21,43 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
-export const MarketplaceFiltersAccordion = () => {
+export const MarketplaceFiltersAccordion = ({ onUpdatedResults }: any) => {
   const { classes } = useStyles();
+  const session = useSession();
+  const [selectedFilters, setSelectedFilters] = useState({});
+
+  const handleFilterButtonClick = ({ title }: any) => {
+    // const title = "Sunset Melodies";
+    // const filters = selectedFilters; // Replace this with your actual filters
+
+    // Make the API request using the title and filters
+    fetch(
+      `http://raidardev.eba-pgpaxsx2.eu-central-1.elasticbeanstalk.com/api/v1/marketplace/songs?title=${encodeURIComponent(
+        title
+      )}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${session.data?.token}`,
+          accept: "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("song", data);
+        onUpdatedResults(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  // const { data: song } = useMarketplaceControllerFindAll({
+  //   queryParams: {
+  //     title: "Sunset Melodies" as string,
+  //   },
+  // });
 
   return (
     <Accordion>
@@ -62,6 +106,13 @@ export const MarketplaceFiltersAccordion = () => {
             <Grid.Col span={4}>2</Grid.Col>
             <Grid.Col span={4}>3</Grid.Col>
           </Grid>
+          <Button
+            onClick={() =>
+              handleFilterButtonClick({ title: "Sunset Melodies" })
+            }
+          >
+            Apply Filters
+          </Button>
         </Accordion.Panel>
       </Accordion.Item>
     </Accordion>
