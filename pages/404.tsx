3b3bc1268e24e08c77fe2e66e78a8e1d6cc06638
dbosnjak94 <dominik.bosnjak94@gmail.com>
2023-08-17 10:23:30 +0200

    404 page - creating initial 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,84 @@
+import {
+  createStyles,
+  Image,
+  Container,
+  Title,
+  Text,
+  Button,
+  SimpleGrid,
+  rem,
+} from "@mantine/core";
+import { useRouter } from "next/router";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  title: {
+    fontWeight: 900,
+    fontSize: rem(34),
+    marginBottom: theme.spacing.md,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(32),
+    },
+  },
+
+  mobileImage: {
+    [theme.fn.largerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  desktopImage: {
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+  button: {
+    backgroundColor: theme.colors.red[5],
+    ...theme.fn.hover({
+      backgroundColor: theme.colors.red[8],
+    }),
+  },
+}));
+
+export function NotFoundImage() {
+  const { classes } = useStyles();
+  const router = useRouter();
+
+  return (
+    <Container className={classes.root}>
+      <SimpleGrid
+        spacing={80}
+        cols={2}
+        breakpoints={[{ maxWidth: "sm", cols: 1, spacing: 40 }]}
+      >
+        <Image src={`/images/404-error.png`} className={classes.mobileImage} />
+        <div>
+          <Title className={classes.title}>Something is not right...</Title>
+          <Text color="dimmed" size="lg">
+            Oops! It seems like we've hit a musical rest in the digital
+            symphony. Our 404 page is singing a solo, and it seems to have lost
+            its way among the notes and codes. Don't worry, though – even the
+            best composers have their off days.
+          </Text>
+          <Button
+            size="md"
+            mt="xl"
+            className={classes.button}
+            onClick={() => router.back()}
+          >
+            Go Back
+          </Button>
+        </div>
+        <Image src={`/images/404-error.png`} className={classes.desktopImage} />
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+export default NotFoundImage;
